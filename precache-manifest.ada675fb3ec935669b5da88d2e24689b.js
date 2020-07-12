623caf6a09862ab36bd7e167cf13bebd40bf991a
mgbenedicto <miguelangelo.benedicto@gmail.com>
2020-07-12 20:52:35 +0800

    Updates

---
+++ precache-manifest.ada675fb3ec935669b5da88d2e24689b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35528d054005329e792f6b9c5e84fdf7",
+    "url": "/mgbenedicto/react-deploy/index.html"
+  },
+  {
+    "revision": "64775a3d27c734c2c8c3",
+    "url": "/mgbenedicto/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/mgbenedicto/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mgbenedicto/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64775a3d27c734c2c8c3",
+    "url": "/mgbenedicto/react-deploy/static/js/main.63845774.chunk.js"
+  },
+  {
+    "revision": "909ab20871aff45d039f",
+    "url": "/mgbenedicto/react-deploy/static/js/runtime-main.e34a1a6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mgbenedicto/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
